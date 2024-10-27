bc2174b0d061a1973e5896962fa1cc86f27045de
eddikan <imeekwere15@gmail.com>
2024-10-27 15:56:35 +0100

    fix: cors error

--- app/api/index.js
+++ app/api/index.js
@@ -1,5 +1,14 @@
-// api/index.js
+// app/api/index.js
 const fastify = require('fastify')();
+const cors = require('fastify-cors');
+
+// Register the CORS plugin
+fastify.register(cors, {
+  // Here you can specify the allowed origins
+  origin: '*', // Allow all origins. Change to a specific domain if needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify any headers you need
+});
 
 // Example route
 fastify.get('/', async (request, reply) => {
